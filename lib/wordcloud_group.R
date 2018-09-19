741f478f266a77b72ed4326d7ccffc26a6a79767
Yiding "Tim" Xie <yx2443@columbia.edu>
2018-09-19 11:07:36 -0400

    Final Update

--- lib/wordcloud_group.R
+++ lib/wordcloud_group.R
@@ -1,6 +1,10 @@
 wordcloud_group <- function(subset){
-  print(subset[1,1])
+  #print(subset[1,1])
+  layout(matrix(c(1, 2), nrow=2), heights=c(1, 4))
+  par(mar=rep(0, 4))
+  plot.new()
+  text(x=0.5, y=0.5, as.character(subset[1,1]))
   wordcloud(words = subset$word, freq = subset$n, min.freq = 1,
             max.words=50, random.order=FALSE, rot.per=0.35, 
-            colors=brewer.pal(8,"Dark2"))
+            colors=rainbow(50), scale = c(3,0.25))
 }
